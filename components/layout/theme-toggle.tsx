2511bc37b45535ccecd66916300dd180c5d9cf6a
Maayan Shalom <sha.maayan@gmail.com>
2024-12-07 14:36:14 +0200

    colorMode === 'light'

--- components/layout/theme-toggle.tsx
+++ components/layout/theme-toggle.tsx
@@ -4,13 +4,13 @@ import { FiMoon, FiSun } from 'react-icons/fi'
 const ThemeToggle = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
-    <IconButton
-      variant="ghost"
-      aria-label="theme toggle"
-      icon={colorMode === 'dark' ? <FiMoon size="14" /> : <FiSun size="14" />}
-      borderRadius="md"
-      onClick={toggleColorMode}
-    />
+      <IconButton
+          variant="ghost"
+          aria-label="theme toggle"
+          icon={colorMode === 'light' ? <FiMoon size="14" /> : <FiSun size="14" />}
+          borderRadius="md"
+          onClick={toggleColorMode}
+      />
   )
 }
 
