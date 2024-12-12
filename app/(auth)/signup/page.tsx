988f8de93b8d8fc71119d88d0553b6fbc6aa883b
Maayan Shalom <sha.maayan@gmail.com>
2024-12-12 21:43:16 +0200

    import fix

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -11,7 +11,6 @@ import { BackgroundGradient } from '#components/gradients/background-gradient'
 import { PageTransition } from '#components/motion/page-transition'
 import { Section } from '#components/section'
 import siteConfig from '#data/config'
-import * as process from "node:process";
 
 const Signup: NextPage = () => {
     const [isMounted, setIsMounted] = useState(false)
